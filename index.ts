537d17a9484dad3c9ee470d57e5591f9135cdd28
agolajko <agoston.lajko@gmail.com>
2025-01-09 11:57:35 +0000

    preset login deatils

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express';
 import { secretKey, createToken, verifyToken } from './token';
 
 const port = 8000;
@@ -12,12 +12,12 @@ app.listen(port, () => {
 })
 
 //public route
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
 // Register user with a POST request
-app.post('/api/register', (req, res) => {
+app.post('/api/register', (req: Request, res: Response) => {
     const { username, password, email } = req.body;
 
     // Check if username or email is taken
@@ -30,36 +30,54 @@ app.post('/api/register', (req, res) => {
     res.send('User registered');
 });
 
-// Login user with a POST request
-app.post('/api/login', (req, res) => {
+const users = [
+    { userId: 1, username: 'admin', password: 'password123' },
+    { userId: 2, username: 'user', password: 'test123' }
+];
+
+app.post('/api/login', (req: Request, res: Response) => {
     const { username, password } = req.body;
 
     // Check if user exists
     // Check if password is correct
     // create token?
 
+    const user = users.find(u =>
+        u.username === username &&
+        u.password === password
+    );
+
+    if (!user) {
+        // console.log("Invalid credentials");
+        res.status(401).json({ message: 'Invalid credentials' });
+        return
+    }
+
     // For now, just logging the received data
     console.log(`Login: username=${username}`);
 
     // Usage example
-    const token = createToken({ userId: 456, username: 'newuser' });
+    const token = createToken({
+        userId: user.userId,
+        username: user.username
+    });
 
     // Return token in response
     res.json({
         message: 'Logged in successfully',
         token: `Bearer ${token}`
     });
+
 });
 
 // protected route
-app.get('/api/protected', (req, res) => {
+app.get('/api/protected', (req: Request, res: Response) => {
     // Check if token is valid
     // If valid, send protected data
     // If not, send unauthorized
 
     // find token from request
     const authHeader = req.headers['authorization'];
-    // Bearer <token>
 
     console.log('Auth header:', authHeader);
 
